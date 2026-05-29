fdd1ede4410f776b69780f1ff1d04cdaa320e4ef
Chikwa-git <lincoln.okoti@gmail.com>
2026-05-28 23:50:30 -0300

    minor fix, removing one import that wasnt being used

--- treinar_modelo.py
+++ treinar_modelo.py
@@ -10,7 +10,6 @@ Dependências:
 """
 
 import pandas as pd
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix
